b9506ff23ee7b11b287ae4fe34bd6adb372dbd11
AJL-1998 <73225992+AJL-1998@users.noreply.github.com>
2022-06-13 19:45:31 +0100

    completed eig_precon

--- backend/conjugate_gradients/preconditioners/Preconditioners.py
+++ backend/conjugate_gradients/preconditioners/Preconditioners.py
@@ -20,11 +20,11 @@ def Eig_Preconditioner(self):
         return None, None, None
     
     if self._q_cache is None:
+        system = self._lazy_tensor.evaluate()
         max_iter = gpytorch.settings.max_preconditioner_size.value()
-        vals_, vecs_ = self._lazy_tensor.symeig(eigenvectors = True)
-        vecs, vals =  lazify(vecs_[:, -max_iter:]), vals_[-max_iter:]
-        L = vecs
-        print(L)
+        vals_, vecs_ = torch.linalg.eigh(system)
+        vecs, vals =  vecs_[:, -max_iter:], vals_[-max_iter:]
+        L = vecs * (vals ** 0.5)
         self._piv_chol_self = L
         if torch.any(torch.isnan(self._piv_chol_self)).item():
             warnings.warn(
@@ -43,14 +43,25 @@ def Eig_Preconditioner(self):
 
     return (precondition_closure, self._precond_lt, self._precond_logdet_cache)
 
-n = 1000
-A = torch.rand(n,n)
-A = A.T @ A
+def rSVD_Preconditioner(self):
+    
+    if gpytorch.settings.max_preconditioner_size.value() == 0 or self.size(-1) < gpytorch.settings.min_preconditioning_size.value():
+        return None, None, None
+    
+    pass
+
+n = 3000
+m = 50
+A = torch.rand(n,m)
+A_ = gpytorch.lazy.RootLazyTensor(A)
 diag = DiagLazyTensor(2. * torch.ones(n))
 rhs = torch.rand(n,1)
 
+import math
+k = int( math.sqrt(n) )
 gpytorch.settings.min_preconditioning_size._set_value(1)
+gpytorch.settings.max_preconditioner_size._set_value(1)
 
-standard_lt = AddedDiagLazyTensor(A, diag)
-overrode_lt = AddedDiagLazyTensor(A, diag, preconditioner_override=Eig_Preconditioner)
-overrode_lt.inv_quad_logdet(rhs)
+standard_lt = AddedDiagLazyTensor(A_, diag)
+overrode_lt = AddedDiagLazyTensor(A_, diag, preconditioner_override=Eig_Preconditioner)
+
